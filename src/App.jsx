4d6863ed371565123e6e6971a52a63e449c33536
Eduardo <el_alienx@hotmail.com>
2022-02-10 09:22:34 +0100

    manage to handle a switch between welcome and shopping screens by using the list state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+// NPM packages
+import { useState } from "react";
+
 // Project files
 import ShoppingScreen from "./screens/ShoppingScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
 
 export default function App() {
+  // Local state
+  const [list, setList] = useState([]);
+
   return (
     <div className="App">
-      <WelcomeScreen />
-      <ShoppingScreen />
+      {list.length === 0 && <WelcomeScreen />}
+      {list.length > 0 && <ShoppingScreen />}
     </div>
   );
 }
