33a48dbcf03bd0d4cbb7f513050de8bfc2ea68c7
Eduardo <el_alienx@hotmail.com>
2022-02-10 09:18:28 +0100

    created the screen components Welcome and Shopping

---
+++ src/pages/ShoppingPage.jsx
@@ -0,0 +1,7 @@
+export default function ShoppingPage() {
+  return (
+    <div>
+      <h1>Shopping list</h1>
+    </div>
+  );
+}
---
+++ src/pages/WelcomeScreen.jsx
@@ -0,0 +1,19 @@
+export default function WelcomeScreen() {
+  return (
+    <div>
+      <img src="" alt="The words EIKA behind a circle background" />
+      <img src="" alt="A woman comming out of a store with 2 shoping bags" />
+      <h1>EIKA's shopping list</h1>
+      <p>
+        Welcome to EIKA’s shopping list. Here you will be able to create a todo
+        list with for the furniture you want to buy.
+      </p>
+      <p>
+        To get started press the Add new item button and a popup will ask you
+        the name and the price of the item you want to add. You can also and an
+        image after the item is added by touching the camera icon.
+      </p>
+      <button>Add item</button>
+    </div>
+  );
+}
