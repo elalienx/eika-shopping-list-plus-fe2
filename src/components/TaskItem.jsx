e3f6b5d535a48fa8a1b6ee546f486ceb357372fc
Eduardo <el_alienx@hotmail.com>
2022-02-16 10:33:04 +0100

    ready to upload to the firebase cloud

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,3 +1,7 @@
+// Project files
+import readFile from "../scripts/readFile";
+import resizeImage from "../scripts/resizeImage";
+
 export default function TaskItem({ item, editList }) {
   const { id, name, price, isCompleted } = item;
 
@@ -9,16 +13,31 @@ export default function TaskItem({ item, editList }) {
     editList(clonedItem);
   }
 
-  function onAddImage() {
-    // something...
+  async function onAddImage(event) {
+    // Step 1
+    const file = event.target.files[0]; // instant
 
-    editList(item);
+    // Step 2
+    const uniqueId = new Date().getTime(); // instant
+    const filename = `thumbnail-${uniqueId}.png`; // instant
+
+    // Step 3
+    const image = await readFile(file); // either 0.5 or 1 seconds or more???
+    const resizedImage = await resizeImage(image, 88, 88);
+
+    // Step 4
+    const imageURL = await uploadImage(resizedImage, filename)
   }
 
   return (
     <li>
       <input type="checkbox" checked={isCompleted} onChange={onCheck} />
       {name}, {price}
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={(event) => onAddImage(event)}
+      />
     </li>
   );
 }
