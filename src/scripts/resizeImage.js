f0d5a2f18af837d0691cb6406a1d4281281c0606
Eduardo <el_alienx@hotmail.com>
2022-02-18 10:26:09 +0100

    perfom a full scale refactoring insdie resizeImage function

--- src/scripts/resizeImage.js
+++ src/scripts/resizeImage.js
@@ -1,49 +1,41 @@
-export default async function resizeImage(image, width = 100, height = 100) {
+export default async function resizeImage(imageSource, width, height) {
   const canvas = document.createElement("canvas");
-  const originalContext = canvas.getContext("2d");
-  const resizedContext = canvas.getContext("2d");
-  const resizedImage = await loadImage(image);
-  const newSize = proportionalScaling(resizedImage, width, height);
-
-  originalContext.drawImage(resizedImage, 0, 0);
+  const context = canvas.getContext("2d");
+  const image = await loadImage(imageSource);
+  const newSize = proportionalScaling(image, width, height);
 
   canvas.width = newSize.width;
   canvas.height = newSize.height;
-  resizedContext.drawImage(resizedImage, 0, 0, newWidth, newHeight);
+  context.drawImage(image, 0, 0, newSize.width, newSize.height);
 
-  return await canvasToPNGImage(canvas);
+  return await canvasToPNGBlob(canvas);
 }
 
 async function loadImage(image) {
-  const resizedImage = new Image();
-  resizedImage.src = image;
+  const result = new Image();
+  result.src = image;
 
-  await new Promise((result) => (resizedImage.onload = result));
+  await new Promise((resolve) => (result.onload = resolve));
 
-  return resizedImage;
+  return result;
 }
 
 function proportionalScaling(image, width, height) {
   let newWidth = image.width;
   let newHeight = image.height;
 
-  // Handles the proportion scaling
   if (newWidth > newHeight) {
-    if (newWidth > width) {
-      newHeight *= width / newWidth;
-      newWidth = width;
-    }
+    newHeight *= width / newWidth;
+    newWidth = width;
   } else {
-    if (newHeight > height) {
-      newWidth *= height / newHeight;
-      newHeight = height;
-    }
+    newWidth *= height / newHeight;
+    newHeight = height;
   }
 
   return { width: newWidth, height: newHeight };
 }
 
-async function canvasToPNGImage(canvas) {
+async function canvasToPNGBlob(canvas) {
   const canvasToDataURL = canvas.toDataURL("image/png");
   const DataURLToBlob = await fetch(canvasToDataURL);
   const result = await DataURLToBlob.blob();
