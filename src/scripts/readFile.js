f69c9e72ae7f0108ea4267153d1bcf29ced34ce0
Eduardo <el_alienx@hotmail.com>
2022-02-18 09:40:07 +0100

    starting to refactor based on the 3r article

--- src/scripts/readFile.js
+++ src/scripts/readFile.js
@@ -1,9 +1,9 @@
 export default async function readFile(file) {
   const reader = new FileReader();
+  reader.readAsDataURL(file);
 
   const result = await new Promise((resolve) => {
     reader.onload = (event) => resolve(event.target.result);
-    reader.readAsDataURL(file);
   });
 
   return result;
