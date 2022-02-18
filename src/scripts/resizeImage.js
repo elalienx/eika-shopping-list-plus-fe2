7a33faa25be985c2e48fa5c4951d1b19748fb673
Eduardo <el_alienx@hotmail.com>
2022-02-18 09:57:15 +0100

    further refactor

--- src/scripts/resizeImage.js
+++ src/scripts/resizeImage.js
@@ -2,16 +2,30 @@ export default async function resizeImage(image, width = 100, height = 100) {
   const canvas = document.createElement("canvas");
   const originalContext = canvas.getContext("2d");
   const resizedContext = canvas.getContext("2d");
-  const resizedImage = new Image();
-  let newWidth = 0;
-  let newHeight = 0;
+  const resizedImage = await loadImage(image);
+  const newSize = proportionalScaling(resizedImage, width, height);
 
-  resizedImage.src = image;
-  await new Promise((result) => (resizedImage.onload = result)); // to force the machine to wait until the image loading is done
-
-  newWidth = resizedImage.width;
-  newHeight = resizedImage.height;
   originalContext.drawImage(resizedImage, 0, 0);
+
+  canvas.width = newSize.width;
+  canvas.height = newSize.height;
+  resizedContext.drawImage(resizedImage, 0, 0, newWidth, newHeight);
+
+  return await canvasToPNGImage(canvas);
+}
+
+async function loadImage(image) {
+  const resizedImage = new Image();
+  resizedImage.src = image;
+
+  await new Promise((result) => (resizedImage.onload = result));
+
+  return resizedImage;
+}
+
+function proportionalScaling(image, width, height) {
+  let newWidth = image.width;
+  let newHeight = image.height;
 
   // Handles the proportion scaling
   if (newWidth > newHeight) {
@@ -26,13 +40,13 @@ export default async function resizeImage(image, width = 100, height = 100) {
     }
   }
 
-  canvas.width = newWidth;
-  canvas.height = newHeight;
-  resizedContext.drawImage(resizedImage, 0, 0, newWidth, newHeight);
+  return { width: newWidth, height: newHeight };
+}
 
+async function canvasToPNGImage(canvas) {
   const canvasToDataURL = canvas.toDataURL("image/png");
   const DataURLToBlob = await fetch(canvasToDataURL);
-  const BlobToFile = await DataURLToBlob.blob();
+  const result = await DataURLToBlob.blob();
 
-  return BlobToFile;
+  return result;
 }
