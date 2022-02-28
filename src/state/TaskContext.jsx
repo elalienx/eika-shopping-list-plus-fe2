0a9ce3677e4183976a0b1e1c0f1d67c78776e4af
Eduardo <el_alienx@hotmail.com>
2022-02-28 09:00:46 +0100

    test

--- src/state/TaskContext.jsx
+++ src/state/TaskContext.jsx
@@ -3,16 +3,21 @@ import { createContext, useState, useContext } from "react";
 
 const Context = createContext(null);
 
+// 1. For the parent (or the person will expose the data to everyone else)
 export function TaskProvider({ children }) {
   // State
   const [tasks, setTasks] = useState([]);
 
-  // Properties
-  const values = { tasks };
+  // Property
+  const values = { tasks } 
+
+  // Methods
+  // inventory of which components are modifying the tasks
 
   return <Context.Provider value={values}>{children}</Context.Provider>;
 }
 
+// 2. For the children that wants to use the data.
 export function useTasks() {
   const context = useContext(Context);
   const errorText =
