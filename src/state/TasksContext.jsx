a33b8b03589cc363a2a05f754131921a2bb00163
Eduardo <el_alienx@hotmail.com>
2022-02-28 11:26:49 +0100

    removed unnecesary comments

--- src/state/TasksContext.jsx
+++ src/state/TasksContext.jsx
@@ -3,7 +3,6 @@ import { createContext, useContext, useState } from "react";
 
 const Context = createContext(null);
 
-// 1. For the parent (or the person will expose the data to everyone else)
 export function TasksProvider({ children }) {
   // State
   const [tasks, setTasks] = useState([]);
@@ -53,7 +52,6 @@ export function TasksProvider({ children }) {
   return <Context.Provider value={values}>{children}</Context.Provider>;
 }
 
-// 2. For the children that wants to use the data.
 export function useTasks() {
   const context = useContext(Context);
   const errorText =
